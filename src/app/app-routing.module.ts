8b23438c6c4b54abb44cf697e17c97d18faf7ab9
Sourabh Waghmare <sourabh15593@mastek.com>
2022-06-01 11:40:22 +0530

    new updated

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,49 @@
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ContactComponent } from './contact/contact.component';
+import { WishlistComponent } from './wishlist/wishlist.component';
+import { AboutComponent } from './about/about.component';
+import { AddtocartComponent } from './addtocart/addtocart.component';
+
+const routes: Routes = [
+  {
+    component:HomeComponent,
+    path:""
+  },
+  {
+    component:LoginComponent,
+    path:"login"
+    
+  },
+  {
+    component:WishlistComponent,
+    path:"wishlis"
+
+  },
+  {
+    component:ContactComponent,
+    path:"contact"
+  },
+  {
+    component:AboutComponent,
+    path:"about"
+  },
+  {
+    component:AddtocartComponent,
+    path:"addtocart"
+  }
+
+  
+
+ 
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)], 
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
